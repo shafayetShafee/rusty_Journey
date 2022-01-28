389a7418a1b1d778c855cb9679eb825d226da1ca
shafayetShafee <seaborn.sicaro@gmail.com>
2022-01-28 23:17:08 +0600

    added gitignore file in guessing game to ignore target folder

--- guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ASUS\Desktop\rust_prog\rusty_Journey\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-C:\Users\ASUS\Desktop\rust_prog\rusty_Journey\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ASUS\Desktop\rust_prog\rusty_Journey\guessing_game\target\debug\guessing_game.exe: C:\Users\ASUS\Desktop\rust_prog\rusty_Journey\guessing_game\src\main.rs
